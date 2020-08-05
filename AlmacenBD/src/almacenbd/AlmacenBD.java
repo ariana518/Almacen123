8a1f5e39d0c60610de1f3c8ca3873a6ec989d9fd
Mis Documentos <Mis Documentos@ARIANA.mshome.net>
2020-08-05 09:46:39 -0500

    primer ejemplo de subir proyecto

---
+++ AlmacenBD/src/almacenbd/AlmacenBD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package almacenbd;
+
+/**
+ *
+ * @author Mis Documentos
+ */
+public class AlmacenBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=2;
+    }
+    
+}
